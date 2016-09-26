a431b8b89b6534584584024bd3666eb9bb48492e
rattanakone <rattanakone@users.noreply.github.com>
2016-09-25 19:32:29 -0700

    testing contact form

--- js/main.js
+++ js/main.js
@@ -1,47 +1,7 @@
-
-var tag = document.createElement('script');
-
-tag.src = "//www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-
-function onYouTubeIframeAPIReady() {
-	player = new YT.Player('ytplayer', {
-		events: {
-			'onReady': onPlayerReady
-		}
-	});
-}
-
-function onPlayerReady(event) {
-	player.playVideo();
-	player.mute();
-//    event.target.setVolume(5);
-//    event.target.playVideo();
-}
-
 new WOW().init();
 
+var contactform =  document.getElementById('contactform');
+    contactform.setAttribute('action', '//formspree.io/' + 'soumontharattanakone' + '@' + 'gmail' + '.' + 'com');
+</script>
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
