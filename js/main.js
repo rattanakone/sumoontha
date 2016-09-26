121c72e01a8948acbac507930466014e432ba9ae
rattanakone <rattanakone@users.noreply.github.com>
2016-09-25 19:36:55 -0700

    trying new form code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 new WOW().init();
 
-var contactform =  document.getElementById('contactform');
-    contactform.setAttribute('action', '//formspree.io/' + 'soumontharattanakone' + '@' + 'gmail' + '.' + 'com');
 
 
 
