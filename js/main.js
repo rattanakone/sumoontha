c7ad7955cd42b7b611b6127364d648f14ad6ce72
rattanakone <rattanakone@users.noreply.github.com>
2016-11-28 21:58:37 -0800

    added portfolio

--- js/main.js
+++ js/main.js
@@ -34,3 +34,15 @@ new WOW().init();
 
 
 
+<script language="javascript">
+document.onmousedown=disableclick;
+status="Right Click Disabled";
+function disableclick(event)
+{
+  if(event.button==2)
+   {
+     alert(status);
+     return false;    
+   }
+}
+</script>
